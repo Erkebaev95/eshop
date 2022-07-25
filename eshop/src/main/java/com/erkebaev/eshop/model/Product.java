917798a9b53f090133af4bea185f0e120d73bc3b
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2022-07-25 22:09:11 +0600

    add anotations @column

--- eshop/src/main/java/com/erkebaev/eshop/model/Product.java
+++ eshop/src/main/java/com/erkebaev/eshop/model/Product.java
@@ -14,11 +14,22 @@ import javax.persistence.*;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "price")
     private int price;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "author")
     private String author;
 
 }
