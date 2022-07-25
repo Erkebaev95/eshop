dbc131dc32310e00df5c6a28a5b11b50319e3327
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2022-07-25 22:07:17 +0600

    add method findByTitle

--- eshop/src/main/java/com/erkebaev/eshop/repository/ProductRepository.java
+++ eshop/src/main/java/com/erkebaev/eshop/repository/ProductRepository.java
@@ -1,8 +1,9 @@
 package com.erkebaev.eshop.repository;
 
+import java.util.List;
 import com.erkebaev.eshop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    List<Product> findByTitle(String title);
 }
