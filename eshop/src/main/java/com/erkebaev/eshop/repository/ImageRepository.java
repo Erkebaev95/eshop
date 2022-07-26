20cea22a25d830db6fa82fd3cf118ec4f53e6fe0
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2022-07-26 21:15:24 +0600

    add interface ImageRepository

---
+++ eshop/src/main/java/com/erkebaev/eshop/repository/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.erkebaev.eshop.repository;
+
+import com.erkebaev.eshop.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
