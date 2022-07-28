08b053503ec0f57854f6d83b350e2c1f8fcbf315
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2022-07-28 19:46:47 +0600

    modifate productInfo, createProduct, deleteProduct and delete method addProduct

--- eshop/src/main/java/com/erkebaev/eshop/controller/ProductController.java
+++ eshop/src/main/java/com/erkebaev/eshop/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.erkebaev.eshop.controller;
 
 import com.erkebaev.eshop.model.Product;
 import com.erkebaev.eshop.services.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,16 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
+@RequiredArgsConstructor
 public class ProductController {
-
     private final ProductService productService;
-
-    @Autowired
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
 
     // List
     @GetMapping("/")
@@ -31,26 +30,25 @@ public class ProductController {
     // info
     @GetMapping("/product/{id}")
     public String productInfo(@PathVariable Long id, Model model) {
-        model.addAttribute("product", productService.getProductById(id));
+        Product product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        model.addAttribute("images", product.getImages());
         return "products-info";
     }
 
     // Add product
-    @GetMapping("/addProduct")
-    public String createProduct(Model model) {
-        model.addAttribute("product", new Product());
-        return "createProduct";
-    }
-
     @PostMapping("/product/create")
-    public String addProduct(Product product) {
-        productService.saveProduct(product);
+    public String createProduct(@RequestParam("file1") MultipartFile file1,
+                                @RequestParam("file2") MultipartFile file2,
+                                @RequestParam("file3") MultipartFile file3,
+                                Product product) throws IOException {
+        productService.saveProduct(product, file1, file2, file3);
         return "redirect:/";
     }
 
     // delete
-    @GetMapping("/product/deleteProduct/{id}")
-    public String deleteProduct(@PathVariable("id") Long id) {
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
         return "redirect:/";
     }
