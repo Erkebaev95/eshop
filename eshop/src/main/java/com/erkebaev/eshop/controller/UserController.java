cbc83ace5d4ceae58775dd4874f05fbc2490ce58
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2022-07-28 21:53:11 +0600

    add user controller

---
+++ eshop/src/main/java/com/erkebaev/eshop/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.erkebaev.eshop.controller;
+
+import com.erkebaev.eshop.model.User;
+import com.erkebaev.eshop.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    // Login
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    // Registration
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String createUser(User user, Model model) {
+        if (!userService.createUser(user)) {
+            model.addAttribute("errorMessage",
+                    "Пользователь с email: " + user.getEmail() +
+                            " уже существует!");
+            return "registration";
+        }
+        //userService.createUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/hello")
+    public String securityUrl() {
+        return "hello";
+    }
+}
